ccdfb668fbd04f6bbf3a5f12b27ff61152cbcfd9
Sakaguchi-Leo <56281605+Sakaguchi-Leo@users.noreply.github.com>
2026-09-15 16:15:31 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()], base: '/contas-casal/' })
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/contas-casal/",
+});
+``
